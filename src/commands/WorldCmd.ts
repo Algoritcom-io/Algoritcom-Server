e321da16c758eea6f90b69b7b46dffabe8fad2a7
Adrian Martinez <adrian.martinez@algoritcom.io>
2024-07-19 23:29:28 +0200

    hotfix?

--- src/commands/WorldCmd.ts
+++ src/commands/WorldCmd.ts
@@ -1,4 +1,5 @@
 import { Command } from "@colyseus/command";
+
 import { AlgoritcomCity } from "../rooms/AlgoritcomCity";
 import { Client } from "colyseus";
 
@@ -14,11 +15,9 @@ export class CmdMovement extends Command<
 
   execute(payload: any) {
     let player = this.state.players.get(payload.client.sessionId);
-    const position = JSON.parse(payload.position);
-    console.log(position);
-    player.position.x = position.x;
-    player.position.y = position.y;
-    player.position.z = position.z;
+    player.position.x = payload.x;
+    player.position.y = payload.y;
+    player.position.z = payload.z;
 
     this.state.players.set(payload.client.sessionId, player);
     return;
