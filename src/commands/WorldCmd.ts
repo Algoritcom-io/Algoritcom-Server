237d28e91ca60c94e10c304df48e7b8fecb5a2bf
Adrian Martinez <adrian.martinez@algoritcom.io>
2024-07-26 19:42:34 +0200

    WIP server to test newCop

--- src/commands/WorldCmd.ts
+++ src/commands/WorldCmd.ts
@@ -2,7 +2,7 @@ import { Command } from "@colyseus/command";
 
 import { AlgoritcomCity } from "../rooms/AlgoritcomCity";
 import { Client } from "colyseus";
-import { AxisData } from "../rooms/schema/PlayerState";
+import { AnimationData, AxisData } from "../rooms/schema/PlayerState";
 
 export class CmdMovement extends Command<
   AlgoritcomCity,
@@ -16,14 +16,18 @@ export class CmdMovement extends Command<
 
   execute(payload: any) {
     let player = this.state.players.get(payload.client.sessionId);
-    console.log("CmdMovement", payload.x, payload.y, payload.z);
     const newPosition = new AxisData();
     newPosition.x = payload.x;
     newPosition.y = payload.y;
     newPosition.z = payload.z;
     player.position = newPosition;
-
-    // this.state.players.set(payload.client.sessionId, player);
+    if (payload.animation) {
+      const newAnimation = new AnimationData();
+      newAnimation.name = payload.animation.name;
+      newAnimation.speed = payload.animation.speed;
+      newAnimation.loop = payload.animation.loop;
+      player.animation = newAnimation;
+    }
     return;
   }
 }
