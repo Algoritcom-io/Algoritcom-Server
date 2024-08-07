ba5d5c09501f54df7c94d7211efbece50e11ecf3
Adrian Martinez <adrian.martinez@algoritcom.io>
2024-08-07 12:16:35 +0200

    add pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+const os = require("os");
+
+/**
+ * COLYSEUS CLOUD WARNING:
+ * ----------------------
+ * PLEASE DO NOT UPDATE THIS FILE MANUALLY AS IT MAY CAUSE DEPLOYMENT ISSUES
+ */
+
+module.exports = {
+  apps: [
+    {
+      name: "colyseus-app",
+      script: "dist/server.js",
+      time: true,
+      watch: false,
+      instances: os.cpus().length,
+      exec_mode: "fork",
+      wait_ready: true,
+      env_production: {
+        NODE_ENV: "dev",
+        PORT: 2567,
+        MAX_PLAYERS: 100,
+      },
+    },
+  ],
+};
